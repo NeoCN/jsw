a2600da223a56b4d53788395d23226e6d5df38d0
mortenson <mortenson@faa2f3f7-6143-4430-a965-5c18bb8f6c17>
2007-04-02 07:33:20 +0000

    Start using a copy of Ant from the user's system rather than supplying our own.

--- wrapper/build32.sh
+++ wrapper/build32.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 
-echo
+echo "--------------------"
 echo "Wrapper Build System"
 echo "--------------------"
 
-if [ "$WRAPPER_TOOLS" = "" ] ; then
-    WRAPPER_TOOLS=tools/apache-ant-1.6.2
-fi
-
-# Make sure our own copy of Ant is used even if the user has defined their own.
-ANT_HOME=$WRAPPER_TOOLS
-export ANT_HOME
-
-chmod u+x $WRAPPER_TOOLS/bin/antRun
-chmod u+x $WRAPPER_TOOLS/bin/ant
-
-$WRAPPER_TOOLS/bin/ant --noconfig -logger org.apache.tools.ant.NoBannerLogger -emacs -Dtools.dir=$WRAPPER_TOOLS -Dbits=32 $@ 
+$ANT_HOME/bin/ant -Dbits=32 $@ 
