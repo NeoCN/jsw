4ecfa448554b212fc0a6c233866bfc552a17730c
mortenson <mortenson@faa2f3f7-6143-4430-a965-5c18bb8f6c17>
2015-11-04 00:37:14 +0000

    Work on 3.5.28

---
+++ wrapper/src/java/org/tanukisoftware/wrapper/event/WrapperSecondInvocationEvent.java
@@ -0,0 +1,45 @@
+package org.tanukisoftware.wrapper.event;
+
+/*
+ * Copyright (c) 1999, 2015 Tanuki Software, Ltd.
+ * http://www.tanukisoftware.com
+ * All rights reserved.
+ *
+ * This software is the proprietary information of Tanuki Software.
+ * You shall use it only in accordance with the terms of the
+ * license agreement you entered into with Tanuki Software.
+ * http://wrapper.tanukisoftware.com/doc/english/licenseOverview.html
+ */
+
+/**
+ * WrapperSecondInvocationEvent is fired whenever a second
+ *  instance of the Wrapper starts in single invocation mode.  
+ *  The property 'wrapper.single_invocation.notify' should also 
+ *  exlicitely set to true.
+ */
+public class WrapperSecondInvocationEvent
+    extends WrapperRemoteControlEvent
+{
+    /*---------------------------------------------------------------
+     * Constructors
+     *-------------------------------------------------------------*/
+    /**
+     * Creates a new WrapperSecondInvocationEvent.
+     */
+    public WrapperSecondInvocationEvent()
+    {
+    }
+    
+    /*---------------------------------------------------------------
+     * WrapperSecondInvocationEvent Methods
+     *-------------------------------------------------------------*/
+    /**
+     * Returns a string representation of the event.
+     *
+     * @return A string representation of the event.
+     */
+    public String toString()
+    {
+        return "WrapperSecondInvocationEvent";
+    }
+}
